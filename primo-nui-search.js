6824c8a9058d9c6b5b26ab85a2357418b223386c
wadeguid <wadeguid@uw.edu>
2023-07-24 12:24:49 -0700

    Updates to 'required field' message
    Updates to 'required field' message

--- primo-nui-search.js
+++ primo-nui-search.js
@@ -6,23 +6,20 @@ function searchSimplePrimo() {
     /* grab the value from queryTemp, and modify it to put in query */
     var temp_query_input = form.querySelector('input[name="queryTemp"]');
     var real_query_input = form.querySelector('input[name="query"]');
+    /*
     real_query_input.value =
-        "any,contains," + temp_query_input.value.replace(/[,]/g, " ");
-    form.submit();
-}
-
-function searchPrimoPfilter() {
-    var form = document.forms["searchFormPrimoNUI"];
-    /* in case the event was triggered on an internal input */
-    while (form.nodeName !== "FORM") form = form.parentElement;
-
-    /* grab the value from queryTemp, and modify it to put in query */
-    var temp_query_input = form.querySelector('input[name="queryTemp"]');
-    var real_query_input = form.querySelector('input[name="query"]');
-    real_query_input.value =
+        "any,contains," + temp_query_input.value.replace(/[,]/g, " "); */
+    if (document.getElementById("pfilter")) {
+        real_query_input.value =
         "any,contains," + temp_query_input.value.replace(/[,]/g, " ") + "&pfilter=" + document.getElementById("pfilter");
+    }
+    else
+       {
+        real_query_input.value =
+        "any,contains," + temp_query_input.value.replace(/[,]/g, " ");
+       }
+    }
     form.submit();
-}
 
 jq(function () {
     jq("#searchFormPrimoNUI").submit(function () {
