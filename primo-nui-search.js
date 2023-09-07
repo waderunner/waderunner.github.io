1e1d4dd5eb96116c3daf02b53c727918349e0838
wadeguid <wadeguid@uw.edu>
2023-09-07 12:25:11 -0700

    updated javascirpt

--- primo-nui-search.js
+++ primo-nui-search.js
@@ -1,25 +1,43 @@
 function searchSimplePrimo() {
-    var form = document.forms["searchFormPrimoNUI"];
-    /* in case the event was triggered on an internal input */
-    while(form.nodeName !== 'FORM')
-       form = form.parentElement;
-    
-    /* grab the value from queryTemp, and modify it to put in query */
-    var temp_query_input = form.querySelector('input[name="queryTemp"]');
-    var real_query_input = form.querySelector('input[name="query"]');
+  var form = document.forms["searchFormPrimoNUI"];
+  /* in case the event was triggered on an internal input */
+  while (form.nodeName !== "FORM") form = form.parentElement;
 
-    real_query_input.value = 'any,contains,' + temp_query_input.value.replace(/[,]/g, " ") ; 
-    
-      
-    form.submit();
- }
+  /* grab the value from queryTemp, and modify it to put in query */
+  var temp_query_input = form.querySelector('input[name="queryTemp"]');
+  var real_query_input = form.querySelector('input[name="query"]');
 
-jq(function() {
- jq("#searchFormPrimoNUI").submit(function(){searchSimplePrimo()});
-// onsubmit
+  real_query_input.value =
+    "any,contains," + temp_query_input.value.replace(/[,]/g, " ");
+
+  form.submit();
+}
+
+jq(function () {
+  jq("#searchFormPrimoNUI").submit(function () {
+    searchSimplePrimo();
+  });
+  // onsubmit
 });
 
+function searchSimplePrimo2() {
+  var form = document.forms["searchFormPrimoNUI"];
+  /* in case the event was triggered on an internal input */
+  while (form.nodeName !== "FORM") form = form.parentElement;
 
+  /* grab the value from queryTemp, and modify it to put in query */
+  var temp_query_input = form.querySelector('input[name="queryTemp"]');
+  var real_query_input = form.querySelector('input[name="query"]');
 
+  real_query_input.value =
+    "any,contains," + temp_query_input.value.replace(/[,]/g, " ");
 
+  form.submit();
+}
 
+jq(function () {
+  jq("#searchFormPrimoNUI2").submit(function () {
+    searchSimplePrimo();
+  });
+  // onsubmit
+});
