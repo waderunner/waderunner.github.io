60e84471b3aee098e92e07ff694a78aa02257111
wadeguid@uw.edu <wadeguid@uw.edu>
2024-12-11 12:13:33 -0800

    scroller height

--- csvmagic/FHL/fhl.js
+++ csvmagic/FHL/fhl.js
@@ -4,7 +4,7 @@ function fhlData() {
         keys: true,
         fixedHeader: true,
         ajax: '/csvmagic/FHL/data/fhl-data-array.json',
-        scrollY: '70%',
+        scrollY: '70vh',
         deferRender: true,
         scroller: true,
         order: [[1, 'asc']],
